aa9dc6676d7438ffbed9e64eaf4a208c4faf3c13
LCoquet <coquet.leo18@gmail.com>
2025-06-19 23:53:41 +0200

    Made the navbar slightly transparent + relative z-10

--- app/components/TopMenu.tsx
+++ app/components/TopMenu.tsx
@@ -10,7 +10,7 @@ export default function TopMenu() {
     const navItemClass = "hover:text-gray-400 dark:hover:text-gray-400 hover:underline transition-all underline-offset-4 duration-300 cursor-pointer";
 
     return (
-        <nav className="text-gray-800 bg-gray-100 shadow">
+        <nav className="text-gray-800 bg-gray-200/40 shadow relative z-10">
             <ul className="flex gap-8 justify-center py-4">
                 {navLinks.map(link => (
                     <li key={link.href}>
