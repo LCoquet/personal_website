761138fcf0492a2f636dc982406fa8eb7ce69bd5
LCoquet <coquet.leo18@gmail.com>
2025-06-19 23:35:31 +0200

    Made MainContainer bg a bit transparent

--- app/components/MainContainer.tsx
+++ app/components/MainContainer.tsx
@@ -6,7 +6,7 @@ type MainContainerProps = {
 
 export default function MainContainer({children}: MainContainerProps ) {
     return (
-        <div className="w-3/4 mx-auto bg-white border border-gray-300 rounded-lg p-8 text-center relative z-10">
+        <div className="w-3/4 mx-auto bg-white/40 border border-gray-300 rounded-lg p-8 text-center relative z-10">
             {children}
         </div>
     );
