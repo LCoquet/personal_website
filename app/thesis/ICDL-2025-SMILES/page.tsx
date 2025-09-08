3fb3e1e41d9f467aebda48c04f9b965b969971e8
LCoquet <coquet.leo18@gmail.com>
2025-09-09 00:01:36 +0200

    Created ICDL 2025 card + page

---
+++ app/thesis/ICDL-2025-SMILES/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Page() {
+    return(
+        <div>
+            Here will be interesting content ngl.
+        </div>
+    )
+}
--- app/thesis/page.tsx
+++ app/thesis/page.tsx
@@ -1,11 +1,18 @@
 'use client';
 
 //imports
+import ArticleCard from "../components/ArticleCard";
 
 export default function Home() {
   return (
     <div>
-      
+      <ArticleCard article={{
+        title: "ICDL 2025 - SMILES Workshop participation",
+        description: "My first participation in an internation conference! I present my work at the SMILES workshop of ICDL 2025.",
+        date: "2025-09-09",
+        link: "/thesis/ICDL-2025-SMILES",
+        image: "/ICDL-2025/ICDL-2025_logo.png"
+      }} />
     </div>
   );
 }
