db8ca4895bb59616a721678ade4b6b73b5f85ed3
LCoquet <coquet.leo18@gmail.com>
2025-09-09 02:06:06 +0200

    Added ICDL-2025-SMILES page, sound and generation steps figure

--- app/thesis/ICDL-2025-SMILES/page.tsx
+++ app/thesis/ICDL-2025-SMILES/page.tsx
@@ -1,9 +1,22 @@
 'use client';
 
+import { useRef } from 'react';
+
 export default function Page() {
-    return(
+    const audioRef = useRef<HTMLAudioElement>(null);
+
+    const handlePlay = () => {
+        audioRef.current?.play();
+    };
+
+    return (
         <div>
-            Here will be interesting content ngl.
+            <h2>SVG Image</h2>
+            <img src="/ICDL-2025/sound_generation_process.svg" alt="Sound generation process, step by step" className='mx-auto my-3 h-[80vh]'/>
+
+            <h2>Sound Reader</h2>
+            <audio ref={audioRef} src="/ICDL-2025/generated_sound.wav" preload="auto" />
+            <button onClick={handlePlay}>Play Sound</button>
         </div>
-    )
+    );
 }
