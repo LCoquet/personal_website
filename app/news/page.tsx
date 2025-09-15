88ee7d97568ce8b4dd4dfeb65f0763f922821a38
LCoquet <coquet.leo18@gmail.com>
2025-09-15 11:31:53 +0200

    Made article cards on three columns on deskto and one on mobile

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -5,13 +5,20 @@ import ArticleCard from "../components/ArticleCard";
 
 export default function Home() {
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
       <ArticleCard article={{
         title: "Website is finally out!",
         description: "After months of procrastination, I finally decided to create my website. I hope I'll be able to add plenty of exciting content as often as possible!",
         date: "2025-06-21",
         link: "/news/new_website",
         image: "/articles_main/placeholder.png"
+      }} />
+      <ArticleCard article={{
+        title: "ICDL 2025 - SMILES Workshop participation",
+        description: "My first participation in an internation conference! I present my work at the SMILES workshop of ICDL 2025.",
+        date: "2025-09-09",
+        link: "/thesis/ICDL-2025-SMILES",
+        image: "/ICDL-2025/ICDL-2025_logo.png"
       }} />
     </div>
   );
--- app/thesis/page.tsx
+++ app/thesis/page.tsx
@@ -9,14 +9,17 @@ export default function Home() {
       My thesis work is about the canary's language learning.
       Canary, as for many other oscine birds, learns its song by imitating adult tutors. The young bird has to listen to the tutor's song, memorize it, and then practice to reproduce it. 
       My current work is to take inspiration from this learning process and knowledge on avian brain to develop a language learning model.
+
       <h2 className='text-5xl font-bold mb-6 mt-6'>Thesis news</h2>
-      <ArticleCard article={{
-        title: "ICDL 2025 - SMILES Workshop participation",
-        description: "My first participation in an internation conference! I present my work at the SMILES workshop of ICDL 2025.",
-        date: "2025-09-09",
-        link: "/thesis/ICDL-2025-SMILES",
-        image: "/ICDL-2025/ICDL-2025_logo.png"
-      }} />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <ArticleCard article={{
+          title: "ICDL 2025 - SMILES Workshop participation",
+          description: "My first participation in an internation conference! I present my work at the SMILES workshop of ICDL 2025.",
+          date: "2025-09-09",
+          link: "/thesis/ICDL-2025-SMILES",
+          image: "/ICDL-2025/ICDL-2025_logo.png"
+        }} />
+      </div>
     </div>
   );
 }
